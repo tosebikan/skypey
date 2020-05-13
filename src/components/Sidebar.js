d678df6b6d8f1fba4255d5c68c07ea1dcc87d952
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-05-13 10:33:03 +0000

    redux restructuring

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Sidebar.css';
-import User from './User';
+import User from '../containers/User';
 const Sidebar = ({contacts}) => {
   return <aside className="Sidebar">
   {contacts.map(contact => <User user={contact} key={contact.user_id} />)}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
-import {contacts} from './static-data';
+import Sidebar from '../components/Sidebar';
+import Main from '../components/Main';
+import {contacts} from '../static-data';
 import _ from 'lodash';
 
 function App() {
