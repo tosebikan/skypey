d09c5ab00518f0f53c6ad1dbea940ea8c651e080
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-05-15 13:11:50 +0000

    Chat Component Complete

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import "./ChatWindow.css";
+import _ from 'lodash'
 import './ChatWindow.css';
 import store from '../store';
 import Header from '../components/Header';
+import Chats from '../components/Chats';
 
 const ChatWindow = ({activeUserId}) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId]
+  const activeMsgs = state.messages[activeUserId];
   return (
     <div className="ChatWindow">
       <Header user={activeUser}/>
+      <Chats messages={_.values(activeMsgs)} />
     </div>
   )
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import contacts from './contacts';
 import user from './user';
 import activeUserId from './activeUserId';
+import messages from './messages'
 
 export default combineReducers({
   contacts,
   user,
-  activeUserId
+  activeUserId,
+  messages
 })
