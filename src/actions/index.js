26aa05edcbd0560c14dc50f818de754fdd487318
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-05-17 12:26:24 +0000

    Chat implemantation complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import {SET_ACTIVE_ID, SET_TYPING_VALUE} from './constants/action-types';
+import {SET_ACTIVE_ID, SET_TYPING_VALUE, SEND_MESSAGE} from './constants/action-types';
 
 export const setActiveUserId = (id) => ({
   type: SET_ACTIVE_ID,
@@ -8,4 +8,12 @@ export const setActiveUserId = (id) => ({
 export const setTypingValue = (value) => ({
   type: SET_TYPING_VALUE,
   payload: value
+})
+
+export const sendMessage = (message, userId) => ({
+  type: SEND_MESSAGE,
+  payload: {
+    message,
+    userId
+  }
 })
--- src/containers/MessageInput.js
+++ src/containers/MessageInput.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './MessageInput.css';
 import store from '../store';
-import {setTypingValue} from '../actions';
+import { setTypingValue, sendMessage } from "../actions";
 
 const MessageInput = ({value}) => {
+
   const handleChange = (e) =>{
     store.dispatch(setTypingValue(e.target.value))
   }
+
+  const state = store.getState();
+
+const handleSubmit = e => {
+    e.preventDefault();
+    const { typing, activeUserId } = state;
+    store.dispatch(sendMessage(typing, activeUserId));
+  };
+
   return (
-    <form className="Message">
+    <form className="Message" onSubmit={handleSubmit}>
       <input
-      className="Message__input"
-      onChange={handleChange}
-      value={value}
-      placeholder="write a message"/>
+        className="Message__input"
+        onChange={handleChange}
+        value={value}
+        placeholder="write a message"
+      />
     </form>
   )
 }
